d3c5b7ed92df3341e8f30ed084a17594e7611322
Marcelo Mogami <marcelomogami@gmail.com>
2026-03-14 17:45:43 -0400

    tirando o debug e ajustando pequenas coisas

--- lib/pessegram/api_listener.rb
+++ lib/pessegram/api_listener.rb
@@ -6,9 +6,7 @@ module Pessegram
   class ApiListener
     def self.start(telegram_bot)
       # Agora a usar a sua nomenclatura padronizada
-      port = (ENV['LISTENER_API_PORT'] || 7355).to_i
       token = ENV['LISTENER_API_TOKEN']
-      puts "DEBUG: O token carregado é: '#{token}'"
 
       # Servidor silencioso para não poluir o terminal do Pessegram
       server = WEBrick::HTTPServer.new(
